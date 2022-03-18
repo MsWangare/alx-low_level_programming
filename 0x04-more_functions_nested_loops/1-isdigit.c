db3224a623df5b8df4d55faafcdbdcc549cdca7c
MsWangare <dorcaswangare0@gmail.com>
2022-03-18 23:04:46 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- *  _isdigit - returns the largest of 3 numbers
- *   @c: char to check if is digit
- *    Return: 1 if is digit case 0 if not 
+ * _isdigit - checks for a digit (0 through 9)
+ * @num: int to be checked
+ * Return: 1 if num is a digit, 0 otherwise
  */
-int _isdigit(int c)
-{	
-	if (c > 47 && c < 58)		
-		return (1);	
-	return (0);
+int _isdigit(int num)
+{
+	return (num >= '0' && num <= '9');
 }
