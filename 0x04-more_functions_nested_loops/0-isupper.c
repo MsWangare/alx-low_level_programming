1ad155c21d10057afeb23daf8cddc9f10a497abc
MsWangare <dorcaswangare0@gmail.com>
2022-03-18 01:23:28 -0700

    Super

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
-#include "holberton.h"
+#include "main.h"
+
 /** 
- * * _isupper - function that checks for uppercase character.
- *  * @c: the int for the paramaters of my function
- *   * Return: 0 
- *   */
+ * _isupper - function that checks for uppercase character.
+ *  @c: the int for the paramaters of my function
+ *   Return: 0 
+ */
 Int _isupper(int c)
 {	
 	     if (c >= 65 && c <= 90)	
