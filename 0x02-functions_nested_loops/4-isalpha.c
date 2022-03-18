667f40b100f8873f2f16d09f0ec1a4faad9d3085
MsWangare <dorcaswangare0@gmail.com>
2022-03-18 06:17:02 -0700

    Is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
- *   *
- *    *@c: The int to print
- *     * Return: Always 0.
- *      */
+ *_isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ *Return: Always 0.
+ */
 
 int _isalpha(int c)
 
