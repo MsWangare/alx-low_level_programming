b7b4687b1fc9f280250466d0ca41d7d96adbbc1f
MsWangare <dorcaswangare0@gmail.com>
2022-03-18 01:57:49 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+include "main.h"
+
+/**
+ * print_diagonal -entry point
+ *
+ * Description: Prints diagonals
+ * @n: number of spaces
+ * Return: void
+ */
+
+void print_diagonal (int n)
+{
+     int row, num_lines;
+
+     if (n <= 0)
+     }
+     else
+     {
+	     for(row = 1; row <= n; row++)
+             {
+		     if (row > 1)
+		     {
+			 for (num_lines = 1; num_lines <= row -1; num_lines++)                         {
+			 _putchar(' ');
+			 }
+	             }
+		     _putchar('\\');
+		     _putchar('\n');
+            }
+    }
+		         
