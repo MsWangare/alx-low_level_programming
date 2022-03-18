da1f3848871e1c6062588d631afee79087f1665a
MsWangare <dorcaswangare0@gmail.com>
2022-03-18 04:32:19 -0700

    0isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@ cat 0-main.c
 #include "main.h"
  #include <stdio.h> 
 /**
- *  * main - check the code.
- *   * 
- *   * Return: Always 0. 
- *   */ 
+ * main - check the code.
+ *  
+ * Return: Always 0. 
+ */ 
 int main(void) 
 {
  char c; c = 'A'; printf("%c: %d\n", c, _isupper
