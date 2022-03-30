792fac38e13ffce17e0e0d80b01ec6b04aa89cd6
MsWangare <dorcaswangare0@gmail.com>
2022-03-30 16:27:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character
  * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1if c is uppercase, 0 otherwise
  */
 int _isupper(int letter)
 {
