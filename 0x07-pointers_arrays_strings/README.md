044c2d2f8712fe9c3dc376c865f22d322e4a253f
MsWangare <dorcaswangare0@gmail.com>
2022-04-07 05:18:13 +0000

    More functions on arrays and strings

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More Functions on Arrays,Pointers and Strings.
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
