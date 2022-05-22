bd0a4073ee976d9a3f82059d9f31e0d07ab2dfd1
MsWangare <dorcaswangare0@gmail.com>
2022-05-22 15:38:43 +0300

    Update 100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	return (1);
+	return (0);
+}
