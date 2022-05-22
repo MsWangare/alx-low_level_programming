cfbd3704d7392c136e6ea0af6090df12ef1d77ae
MsWangare <dorcaswangare0@gmail.com>
2022-05-22 15:32:56 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,37 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - reads text file and prints to POSIX
+ * @filename: file to read from
+ * @letters: letters to be read
+ * Return: number of letters.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd, wrt, rd;
+	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * (letters));
+	if (buff == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	rd = read(fd, buff, letters);
+	wrt = write(STDOUT_FILENO, buff, rd);
+	if (wrt == -1)
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
+	free(buff);
 
-	free(buf);
-
-	return (nwr);
+	return (wrt);
 }
