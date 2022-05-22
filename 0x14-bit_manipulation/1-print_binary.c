f138217212c47d5e4260b73e3376ce95d91ab902
MsWangare <dorcaswangare0@gmail.com>
2022-05-22 15:48:31 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,32 @@
-_print_rev_recursion - Prints a string in reverse
+#include "main.h"
+
+/**
+ * print_binary - prints binary
+ * @n: int to print
+ *
+ * Return: void function.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int count = 0;
+	unsigned long int copy = n;
+	int i;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+
+	while (n)
+	{
+		count++;
+		n >>= 1;
+	}
+
+	n = copy;
+
+	for (i = count - 1; i >= 0; i--)
+		_putchar(48 + ((n >> i) & 1U));
+}
