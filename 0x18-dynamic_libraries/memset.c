af51e2efa51b647b3805a6e45dba42d97001bc7e
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:48:04 +0300

    6. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/memset.c
@@ -0,0 +1,19 @@
+
+
+#include "main.h"
+
+/**
+ * _memset - man memset
+ * @s: man memset
+ * @b: man memset
+ * @n: man memset
+ *
+ * Return: man memset
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (b != 0)
+		n++;
+	return (s);
+}
