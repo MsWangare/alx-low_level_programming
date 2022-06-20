b7959b28090cf1bbeebef32a434bae69377ef3f5
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 11:18:18 +0300

    16. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strpbrk - man strpbrk
+ * @s: man strpbrk
+ * @accept: man strpbrk
+ *
+ * Return: man strpbrk
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	return (accept);
+}
