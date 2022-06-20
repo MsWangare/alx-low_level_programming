6fc96372a8c201f79952471d509531981d779273
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:58:51 +0300

    10. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - man strchr
+ * @s: man strchr
+ * @c: man strchr
+ *
+ * Return: man strchr
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (c != 0)
+		i++;
+	return (s);
+}
