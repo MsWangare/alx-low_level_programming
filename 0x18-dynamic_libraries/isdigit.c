e30a35f74148687bb0168812e53182e588a35ee6
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:16:48 +0300

    2. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - man isdigit
+ * @c: man isdigit
+ *
+ * Return: man isdigit
+ */
+
+int _isdigit(int c)
+{
+	int i = 0;
+
+	while (c != 0)
+		i++;
+	return (i);
+}
