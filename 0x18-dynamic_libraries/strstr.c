d1650f6fcc90bff7987a56563ce3c1da15e17bcc
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 11:44:04 +0300

    18. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strstr - man strstr
+ * @haystack: man strstr
+ * @needle: man strstr
+ *
+ * Return: man strstr
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (haystack[i] != 0)
+		i++;
+	return (needle);
+}
