76dbc4e16cff9e7667d4492b727c02e8497d0732
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:56:57 +0300

    9. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - man strcat
+ * @dest: man strcat
+ * @src: man strcat
+ *
+ * Return: man strcat
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != 0)
+		i++;
+	return (src);
+}
