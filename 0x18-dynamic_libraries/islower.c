a58f059662b6dbad04dcccf119a65beb2bcf5669
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:38:13 +0300

    3. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - man islower
+ * @c: man islower
+ *
+ * Return: man islower
+ */
+
+int _islower(int c)
+{
+	int i = 0;
+
+	while (c != 0)
+		i++;
+	return (i);
+}
