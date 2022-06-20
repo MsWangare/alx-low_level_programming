59a2407099d1863459ecc24a47a5954c931f4fcb
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 11:10:34 +0300

    12. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - man strcpy
+ * @dest: man strcpy
+ * @src: man strcpy
+ *
+ * Return: man strcpy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != 0)
+		i++;
+	return (src);
+}
