2d7faf25312807fe3df0f096cc9236a5438aefaf
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:40:48 +0300

    4. Without libraries what have we? We have no past and no future [5]

---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,19 @@
+
+
+#include "main.h"
+
+/**
+ * _isupper - man _isupper
+ * @c: man _isupper
+ *
+ * Return: man _isupper
+ */
+
+int _isupper(int c)
+{
+	int i = 0;
+
+	while (c != 0)
+		i++;
+	return (i);
+}
