695f39af65ec38269d617cda4116c6216d9f0b17
MsWangare <dorcaswangare0@gmail.com>
2022-06-20 10:15:04 +0300

    1. Without libraries what have we? We have no past and no future

---
+++ 0x18-dynamic_libraries/isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - man isalpha
+ * @c: man isalpha
+ *
+ * Return: man isalpha
+ */
+
+int _isalpha(int c)
+{
+	int i = 0;
+
+	while (c != 0)
+		i++;
+	return (i);
+}
